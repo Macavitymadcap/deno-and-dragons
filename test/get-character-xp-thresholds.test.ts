4b7245198a257ceb5fd021df0897c1601bc34b9b
dankbjss <dan.kiernan@bjss.com>
2024-10-16 20:08:10 +0100

    Initial commit

---
+++ test/get-character-xp-thresholds.test.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from 'jsr:@std/assert';
+import { getCharacterXPThresholds } from "../src/functions/get-xp-thresholds.ts";
+import { Level, XP_THRESHOLDS_BY_LEVEL } from "../src/functions/model.ts";
+
+
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20].forEach((level) => {
+    Deno.test(`getCharacterXPThresholds given a ${level} returns the correct XP thresholds`, () => {
+        // Arrange
+        const expectedThreshold = XP_THRESHOLDS_BY_LEVEL[level as Level];
+        
+        // Act
+        const result = getCharacterXPThresholds(level as Level);
+
+        // Assert
+        assertEquals(result, expectedThreshold);
+    });
+})
