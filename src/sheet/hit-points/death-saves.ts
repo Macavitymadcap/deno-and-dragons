58ffee345d9a9778d951722722b69f9f10afae57
Macavitymadcap <danielthekiernan@gmail.com>
2024-10-19 17:16:35 +0100

    started fleshing out hit points

---
+++ src/sheet/hit-points/death-saves.ts
@@ -0,0 +1,28 @@
+import { IDeathSaves } from "./hit-points.model.ts";
+import { Dice, DiceRoll, RollType } from "../../dice/index.ts";
+import { XP_THRESHOLDS_BY_LEVEL } from "../../encounter-evaluator/encounter-evaluator.model.ts";
+
+export class DeathSaves implements IDeathSaves {
+    successes!: number;
+    failures!: number;
+    private d20 = 'd20';
+
+    constructor(deathSaves: IDeathSaves) {
+        this.successes = deathSaves.successes;
+        this.failures = deathSaves.failures;
+    }
+
+    roll(rollType?: RollType): DiceRoll {
+        const roll = new Dice(this.d20).roll(rollType);
+
+        if (roll.total >= 11) {
+            this.successes = Math.min(3, this.successes + 1);
+        }
+        else {
+            this.failures = Math.min(3, this.failures + 1);
+        }
+
+        return roll;
+    }
+
+}
--- src/sheet/hit-points/hit-points.model.ts
+++ src/sheet/hit-points/hit-points.model.ts
@@ -1,11 +1,13 @@
+export interface IDeathSaves {
+    successes: number;
+    failures: number;
+}
+
 export interface IHitPoints {
     hitPointMaximum: number;
     currentHitPoints: number;
     temporaryHitPoints: number;
     hitDiceTotal: string;
     hitDiceCurrent: string;
-    deathSaves: {
-        successes: number;
-        failures: number;
-    }
+    deathSaves: IDeathSaves;
 }
