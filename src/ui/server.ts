904c4c427af08b28c140598a94c0dae027b65d7b
dankbjss <dan.kiernan@bjss.com>
2024-10-21 12:46:57 +0100

    using path relative to base in server and sort symbol on initiative table

--- src/ui/server.ts
+++ src/ui/server.ts
@@ -1,7 +1,8 @@
 import { serveDir } from "@std/http/file-server";
+import * as path from "jsr:@std/path";
 
 Deno.serve(async (req) => {
-  const rootPath = "/home/dank/Code/deno-and-dragons/build";
+  const rootPath = `${Deno.cwd()}/build`;
   const url = new URL(req.url);
   const pathname = decodeURIComponent(url.pathname);
 
